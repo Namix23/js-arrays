67ab3d3729314733ab9f89be7061d00c4757d183
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-11 22:57:00 -0400

    create array, initialize

---
+++ script.js
@@ -0,0 +1,30 @@
+//Basic Data Types
+
+//string
+//number
+//bigInt
+//boolean
+//undefined
+//null
+//symbol
+
+//Arrays
+let myArray = [50, 10, 30];
+myArray.push(100);
+console.log(myArray);
+
+myArray.pop();
+console.log(myArray);
+
+myArray.sort();
+console.log(myArray);
+
+let fruits = ["apple", "banana", "orange"];
+fruits.sort();
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+fruits.pop();
+fruits.toSorted();
+console.log(fruits);
